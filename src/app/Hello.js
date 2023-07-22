179e9e2744ab1ea6ec2bd733a4ba1b81c25b8c53
Nagaraj Yallurkar <89789184+NAGARAJYALLURKAR@users.noreply.github.com>
2023-07-22 11:38:31 +0530

    Add files via upload

---
+++ src/app/Hello.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Hello = () => {
+  return (
+    <div>
+    <h1>
+
+      this is first folder
+    </h1>
+      <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse optio
+        quibusdam maxime fuga dolore aliquid animi voluptates quae nemo
+        voluptatibus voluptatem, quidem repudiandae voluptatum nesciunt sint
+        similique harum, laudantium doloribus.
+      </div>
+      <div>
+        I am going to upload this on netlify
+      </div>
+    </div>
+  );
+};
+
+export default Hello;
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+
+import Hello from './Hello'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+    <Hello/>
+    </main>
+  )
+}
